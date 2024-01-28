b7735e55e1bbdad387dd94f9b426082c1852f7f7
Viktoriia <pecourses>
2024-01-28 15:42:39 +0200

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,36 @@
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+import CreatePetPage from './pages/CreatePetPage';
+import PetsListPage from './pages/PetsListPage';
+
 function App () {
-  return <div className='App'></div>;
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/pet/create'>Add Pet</Link>
+          </li>
+          <li>
+            <Link to='/pets'>Pets List</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path='/' exact>
+          Home
+        </Route>
+        <Route path='/pet/create'>
+          <CreatePetPage />
+        </Route>
+        <Route path='/pets'>
+          <PetsListPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
